f5fd2275f32b952c1f87a654ea3e75a9177d6244
Joshua Vand der Most <joshua.vandermost@cambriancollege.ca>
2022-11-03 18:22:33 -0400

    added a detailed view

---
+++ ContactApp/ContactDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ContactDetailsViewController.swift
+//  ContactApp
+//
+//  Created by Cambrian on 2022-11-03.
+//
+
+import UIKit
+
+class ContactDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
